8cd733f8b060ebd75bd87d702ae1dea5e7077670
CaioViniF <caiovini06@gmail.com>
2025-05-10 18:26:36 -0300

    Atualização do cliente/servidor

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+    res.json({ message: "Olá, cliente!" });
+});
+
+app.post('/api', (req, res) => {
+    res.json({ received: req.body });
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando em http://localhost:${3000}");
+});
